66daec7e2b74d99066e6b200e011d7bc81ce3950
Aryagorjipour <arygorji.pour@gmail.com>
2021-09-13 13:33:10 +0430

    first commit

---
+++ src/GameFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+
+public class GameFrame extends JFrame{
+
+    GameFrame() {
+
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setResizable(false);
+        //this.setLocationRelativeTo(null);
+        this.pack();
+        this.setVisible(true);
+    }
+    
+}
